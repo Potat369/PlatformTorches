89731a6ee67ef94a7d455b51420ada62f177552c
yevhenii <yevheniidemian@gmail.com>
2024-10-22 17:03:42 +0300

    Initial commit

---
+++ PlatformTorches.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Reflection;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PlatformTorches
+{
+	public class PlatformTorches : Mod
+	{
+		public override void Load()
+		{
+			
+		}
+	}
+}
