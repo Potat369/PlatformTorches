a63641566de647af75ac2ffa0f588a5e9864b765
yevhenii <yevheniidemian@gmail.com>
2024-10-22 17:04:24 +0300

    Add IL modification to modify original check

--- PlatformTorches.cs
+++ PlatformTorches.cs
@@ -13,7 +13,31 @@ namespace PlatformTorches
 	{
 		public override void Load()
 		{
-			
+			IL_SmartCursorHelper.Step_Torch += HookStepTorch;
+		}
+
+		private void HookStepTorch(ILContext il)
+		{
+			try
+			{
+				var c = new ILCursor(il);
+				
+				if (c.TryGotoNext(i => i.MatchBrfalse(out _), i => i.MatchLdsfld(typeof(Main).GetField("tileNoAttach")), i => i.MatchLdloca(10)))
+				{
+					c.GotoNext();
+					c.EmitLdloca(10);
+					c.EmitCall(typeof(Tile).GetProperty("type", BindingFlags.NonPublic | BindingFlags.Instance).GetGetMethod(true));  // Get Tile.type
+					c.EmitLdindU2();
+					c.EmitLdcI4(TileID.Platforms);
+					c.Emit(OpCodes.Ceq);
+					c.EmitBrtrue(c.Next.Next.Next.Next.Next.Next.Next);
+				}
+				MonoModHooks.DumpIL(ModContent.GetInstance<PlatformTorches>(), il);
+			}
+			catch (Exception e)
+			{
+				MonoModHooks.DumpIL(ModContent.GetInstance<PlatformTorches>(), il);
+			}
 		}
 	}
 }
